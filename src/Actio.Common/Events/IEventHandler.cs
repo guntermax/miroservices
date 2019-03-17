3f3b831da593b17538772f1925d88dde25a2969e
Maxim Gunter <gunter.maxim@gmail.com>
2019-03-17 11:42:08 -0400

    Added Events

---
+++ src/Actio.Common/Events/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actio.Common.Events
+{
+    interface IEventHandler<in T> where T : IEvent
+    {
+        Task HandleAsync(T @event);
+    }
+  
+}
